fb02fea0581768b1c996f2cf85ee543b5be3bafa
Sandeep Kumar <sandeepkumar073113@gmail.com>
2024-12-05 14:08:32 +0530

    Conversion in js

---
+++ conversion.js
@@ -0,0 +1,36 @@
+//implicit converison
+/*
+//1 string to number
+console.log("2"-2);//0
+console.log("5"*2);//10
+
+
+//2 number to string
+console.log("123"+2); //1232
+
+//3 boolean to number
+console.log(true+1);//2;
+
+
+
+/// explicit conversion
+
+//1 conversion to string
+//a  string()
+console.log(String(123));
+console.log(typeof (String(123)));
+
+//b tostring();
+let num=1234;
+console.log(num.toString());
+*/
+
+
+//2 conversion to number 
+//a number()
+console.log(Number(123));
+console.log(typeof (Number(123)));
+
+//b tostring();
+let num="1234";
+console.log(parseFloat(num));
